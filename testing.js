0a99a1f827c5706b8933b45a9693dca210205d1a
Varunjoshi-19 <varunjoshi6283@gmail.com>
2025-08-17 13:04:33 +0530

    new change bro

--- testing.js
+++ testing.js
@@ -3,3 +3,5 @@ const value = 100;
 console.log("this is the testing program using jenkins" , value);
 console.log("changes should be reflected whenever you change");
 console.log("this is the new change bro");
+
+print(""!5);
